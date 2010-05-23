90a80ab85637a7f568efb7000dee4d4acc8ddc01
Joel Parker Henderson <joel@sixarm.com>
2010-05-23 16:14:47 -0700

    first commit

---
+++ test/sixarm_ruby_action_controller_mock_test.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require 'sixarm_ruby_action_controller_mock'
+
+class Testing < Test::Unit::TestCase
+
+ def setup
+  @mock=ActionController::Base.new
+ end
+
+ def test_rails_root_constant
+  assert_equal('',ActionController::Base::RAILS_ROOT,"mock rails root")
+ end
+
+ def test_filter_chain_empty
+  assert_equal([],@mock.filter_chain)
+ end
+
+ def test_before_filter
+  @mock.before_filter(:foo)
+  assert_equal([:foo],@mock.filter_chain)
+ end
+
+ def test_after_filter
+  @mock.after_filter(:foo)
+  assert_equal([:foo],@mock.filter_chain)
+ end
+
+end
